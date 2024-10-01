299a3b18b2d5bfefbc9a0e6b482a9767cc8787f2
Mauricio Bedoya <mauricio.bedoya@gmail.com>
2024-10-01 13:58:47 -0500

    GRA-001: JWT Support - Top Level from userid (id)

--- src/app.js
+++ src/app.js
@@ -1,59 +1,92 @@
-const express = require('express');
-const cors = require('cors');
-const { json } = require('body-parser');
-const fs = require('fs');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-const parse = require('csv-parse/lib/sync');
+const express = require("express");
+const cors = require("cors");
+const { json } = require("body-parser");
+const fs = require("fs");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const jwt = require("jsonwebtoken");
 
-const app = express()
-  .use(cors())
-  .use(json());
+const parse = require("csv-parse/lib/sync");
 
-console.log('Getting schemas...');
+const app = express().use(cors()).use(json());
 
-const schema = buildSchema(fs.readFileSync('graphql/schema.graphql', 'utf8'));
+// JWT middleware (Decoding only, no verification)
+app.use((req, res, next) => {
+    const token = req.headers["authorization"];
+    
+    if (token) {
+        try {
+            // Decode the JWT token without verifying
+            const decodedToken = jwt.decode(token.replace("Bearer ", ""));
 
-console.log('Getting data...');
-const groups = parse(fs.readFileSync('data/groups.csv', 'utf8'), { columns: true });
-const repos = parse(fs.readFileSync('data/repos.csv', 'utf8'), { columns: true });
+            if (decodedToken) {
+                // Attach the decoded token to the request object
+                req.authPayload = decodedToken;        
+            }
+        
+        } catch (error) {
+            console.log("Error decoding token");
+        }
+    }
+    next();
+});
 
-console.log('Setting up API root...');
+console.log("Getting schemas...");
+
+const schema = buildSchema(fs.readFileSync("graphql/schema.graphql", "utf8"));
+
+console.log("Getting data...");
+const groups = parse(fs.readFileSync("data/groups.csv", "utf8"), {
+    columns: true,
+});
+const repos = parse(fs.readFileSync("data/repos.csv", "utf8"), {
+    columns: true,
+});
+
+console.log("Setting up API root...");
 
 const root = {
-  groups: (args) => {
-    if (args.parentGroupPath === undefined) {
-        return groups.filter(group => group.parentgroup === "");
-    }
+    groups: (args, context) => {
+        if (args.parentGroupPath === undefined) {
+            return groups.filter((group) => group.parentgroup == context.authPayload.uid);
+        }
 
-    if (args.parentGroupPath) {
-        return groups.filter(group => group.parentgroup === args.parentGroupPath);
-    }
+        if (args.parentGroupPath) {
+            return groups.filter(
+                (group) => group.parentgroup === args.parentGroupPath
+            );
+        }
 
-    throw new Error("Error getting Groups");
-  },
-  repos: (args) => {
-    if (args.parentGroupPath === undefined) {
-        throw new Error("Parent Group can not be empty");
-    }
+        throw new Error("Error getting Groups");
+    },
+    repos: (args, context) => {
+        if (args.parentGroupPath === undefined) {
+            return repos.filter((repo) => repo.parentgroup == context.authPayload.uid);
+        }
 
-    if (args.parentGroupPath) {
-        return repos.filter(repo => repo.parentgroup === args.parentGroupPath);
-    }
+        if (args.parentGroupPath) {
+            return repos.filter((repo) => repo.parentgroup == args.parentGroupPath);
+        }
 
-    throw new Error("Error getting Repos");
-  }
+        throw new Error("Error getting Repos");
+    },
 };
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+    "/graphql",
+    graphqlHTTP((req) => ({
+        schema,
+        context: {
+            authPayload: req.authPayload,
+        },
+        rootValue: root,
+        graphiql: { headerEditorEnabled: true },
+    }))
+);
 
 app.listen(4201, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  return console.log('Server listening on port 4201');
+    if (err) {
+        return console.log(err);
+    }
+    return console.log("Server listening on port 4201");
 });
